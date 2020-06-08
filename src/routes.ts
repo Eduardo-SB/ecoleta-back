39843ed41a0a01b8e7ae6543b7e0a758a0291a27
Eduardo SB <eduardosb95@gmail.com>
2020-06-07 22:16:31 -0300

    completed endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 
-const routes = express.Router();
+import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsController';
 
-routes.get('/', (req, res) => {
-  return res.json({message: "Server is running"})
-});
+// index = list objects , show = show 1 object, create = create , update = update, delete = delete
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itensController = new ItemsController();
+
+routes.get('/items', itensController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
